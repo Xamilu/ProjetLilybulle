95dda340fe4ca91c3dc18c3bfa06391d193d39e6
Labyrinthegreg <leo.gregori95@gmail.com>
2021-05-04 16:53:42 +0200

    ajout d'un check lorsque l'email s'envoie

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -5,13 +5,11 @@ const telForm = document.querySelector("#tel")
 const objetMail = document.querySelector("#motif-select")
 const msgMail = document.querySelector("#msg")
 
-document.querySelector('#submit').addEventListener('click', (event) => {
-   // Set the IDs
-    // 🚫 INSECURE 🚫 //
-    const SERVICE_ID = 'service_4s4qmmf'
-    const TEMPLATE_ID = 'template_v42qoim'
-    const USER_ID = 'user_lmDYGxw2cx0QPO420I7IY'
+document.querySelector(".checkbox input").disabled = true;
 
+document.querySelector('#submit').addEventListener('click', (event) => {
+
+   event.preventDefault()
 
    let params = {
       nameSend: nameForm.value.toUpperCase(),
@@ -20,13 +18,45 @@ document.querySelector('#submit').addEventListener('click', (event) => {
       objectMail: objetMail.value,
       tel: telForm.value,
       mel: mailForm.value
-   };
+   }
 
-   /* emailjs.send(SERVICE_ID, TEMPLATE_ID, params, USER_ID)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    }) */
+   if (checkInputs()) {
+      // emailjs.send("service_4s4qmmf", "template_v42qoim", params, "user_lmDYGxw2cx0QPO420I7IY")
+
+      nameForm.value = ''
+      prenomForm.value = ''
+      msgMail.value = ''
+      objetMail.value = "--Choix de l'Objet--"
+      telForm.value = ''
+      mailForm.value = ''
+      document.getElementById('compteur').innerHTML = msg.value.length;
+
+      document.querySelector(".checkbox input").checked = true;
+   }
 })
-;
+
+function checkInputs() {
+   let emailRegexp = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+   let telRegexp = /^[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}$/
+   if (nameForm.value.length < 2) {
+      return false
+   }
+   else if (prenomForm.value.length < 2){
+      return false
+   }
+   else if (!emailRegexp.test(mailForm.value)){
+      return false
+   }
+   else if (msgMail.value.length < 50){
+      return false
+   }
+   else if (!telRegexp.test(telForm.value)) {
+      return false
+   }
+   else if (objetMail.value == "--Choix de l'Objet--") {
+      return false
+   }
+   else {
+      return true
+   }
+}
