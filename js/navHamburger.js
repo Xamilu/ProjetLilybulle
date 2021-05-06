2055e3d8c31ff115cc28d1abfd6c7025be6e8425
Labyrinthegreg <leo.gregori95@gmail.com>
2021-05-06 15:47:28 +0200

    Ajout icone menu burger

--- js/navHamburger.js
+++ js/navHamburger.js
@@ -15,7 +15,7 @@ span.onclick = function(){
             menu.style.animation = "bgBurgerOpen 0.6s ease-in-out forwards"
             setTimeout(() => {
                 links.style.opacity = "1"
-                links.style.left = "27%"
+                links.style.left = "20%"
                 span.style.opacity = "1"
             }, 300);
         }, 600);
