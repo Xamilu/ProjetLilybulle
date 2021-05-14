2284508c2167692884a4eb3a398f46d5fb154167
Léo <leo.gregori95@gmail.com>
2021-05-14 15:49:00 +0200

    Ajout page pour annuler l'abonnement

--- public/js/resetEmail.js
+++ public/js/resetEmail.js
@@ -2,11 +2,10 @@ const url = window.location.search
 const urlParams = new URLSearchParams(url)
 const emailId = urlParams.get('id')
 
-const emailContainer = document.querySelector("#email") 
-
-emailContainer.innerHTML = emailId
 let options = {
     method: 'POST',
     body: emailId
 }
-fetch('/db/deleteEmail', options).then()
+// window.onload = async () => {
+//     await fetch('/db/deleteEmail', options).then()
+// }
